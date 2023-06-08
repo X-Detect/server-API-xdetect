17e1c3018c0fba03a17757a09827f1ccccdb3816
Farhan Aly <73029205+alyhasbi@users.noreply.github.com>
2023-06-08 08:17:20 +0700

    Adding Dockerfile, routes, and handler

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const routes = require('./routes');
+require('dotenv').config();
+
+const app = express();
+const port = 8080;
+
+app.use(express.json({ limit: '10mb' }));
+
+// app.use(routes);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server berjalan pada http://localhost:${port}`);
+});
