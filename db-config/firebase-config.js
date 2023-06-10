bb84d555f2f0da30db03493072fffcbae0bf8a07
Farhan Aly <73029205+alyhasbi@users.noreply.github.com>
2023-06-10 20:06:01 +0700

    Using body-parser (MD request)

--- db-config/firebase-config.js
+++ db-config/firebase-config.js
@@ -5,14 +5,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// const firebaseConfig = {
+//     apiKey: process.env.API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     projectId: process.env.PROJECT_ID,
+//     storageBucket: process.env.STORAGE_BUCKET,
+//     messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//     appId: process.env.APP_ID,
+// };
+
 const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-};
+    apiKey: "AIzaSyBIFI1I9MvGe-5EUn1cnx42ccm67GILmNU",
+    authDomain: "xdetect-api.firebaseapp.com",
+    projectId: "xdetect-api",
+    storageBucket: "xdetect-api.appspot.com",
+    messagingSenderId: "1097004825071",
+    appId: "1:1097004825071:web:e1a632da75e7e22c8f5e1a"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
