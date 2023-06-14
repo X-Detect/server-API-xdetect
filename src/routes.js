40f6d1ae10e554535b5f546a61e9e5cd666457b5
Farhan Aly <73029205+alyhasbi@users.noreply.github.com>
2023-06-14 19:03:08 +0700

    Fixing routes and documentation

--- src/routes.js
+++ src/routes.js
@@ -6,18 +6,23 @@ const router = express.Router();
 const multerStorage = multer.memoryStorage();
 const upload = multer({ storage: multerStorage });
 
+// Routes for users
 router.post('/signup', signUp)
 router.post('/signin', signIn)
-router.post('/reset-password', resetPassword)
 router.post('/signout', signOutUser)
-router.post('/predict', upload.single('image'), predict)
+router.post('/reset-password', resetPassword)
+router.get('/users', getUsers)
+router.get('/users/:uid', getUserUid)
 router.post('/upload-profile-picture', upload.single('image'), uploadProfilePicture);
 router.put('/upload/:uid', upload.single('image'), uploadProfilePictureWithUID);
+
+// Routes for prediction
+router.post('/predict', upload.single('image'), predict)
+
+// Routes for article
 router.post('/article', postArticle)
 router.get('/article', getAllArticles)
 router.get('/article/:uid', getArticleByUID)
-router.get('/users', getUsers)
-router.get('/users/:uid', getUserUid)
 
 
 
